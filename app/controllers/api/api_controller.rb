3b8b94a7b0b5c1486dc55cc496bacd99b726587a
Kyle Pollich <kpollich1@gmail.com>
2016-01-08 10:07:19 -0500

    Add basic auth back to API layer.

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,5 +1,17 @@
 module Api
   class ApiController < JSONAPI::ResourceController
+    before_action :authenticate
     protect_from_forgery with: :null_session
+
+    protected
+
+    def authenticate
+      if user = authenticate_with_http_basic { |u, p| Player.find_by(name: u).authenticate(p) }
+        @current_user = user
+      else
+        render :nothing => true, :status => :unauthorized
+      end
+    end
+
   end
 end
